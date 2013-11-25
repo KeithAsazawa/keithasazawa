e1630b1e08a50353f94e9dbbc581c922f7283fd2
KeithAsazawa <keith.asazawa@gmail.com>
2013-11-25 12:07:05 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keithasazawa::Application.config.secret_token = '1218dc1d8e4eb24dbcc55c4c0089507ebb32795024c29379704aecc8d4739d89401f309c8ffb092375c4fc63eee64ecd7634525039aa7df5a14a0aee13cdc727'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Keithasazawa::Application.config.session_store :cookie_store, key: '_keithasazawa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Keithasazawa::Application.config.session_store :active_record_store
